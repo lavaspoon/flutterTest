23b8a8e450fe8816e103c6eb01f732d98f5fc414
lavaspoon <lavaspoon@naver.com>
2022-07-28 15:05:04 +0900

    병합

--- flutter_application_1/lib/pages/lava_home_page.dart
+++ flutter_application_1/lib/pages/lava_home_page.dart
@@ -6,50 +6,15 @@ class LavaHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("LavaHome"),
+      appBar: AppBar(
+        title: const Text("AppBar MyHomePage"),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 5, left: 10, right: 10),
+        child: Container(
+          color: Colors.green,
         ),
-<<<<<<< HEAD
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            color: Colors.green,
-            height: 200,
-            width: 200,
-          ),
-=======
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              color: Colors.red,
-              alignment: Alignment.center,
-              width: 100,
-              height: 20,
-              child: const Text(
-                "1",
-              ),
-            ),
-            Container(
-              color: Colors.green,
-              alignment: Alignment.center,
-              width: 100,
-              height: 30,
-              child: const Text(
-                "1",
-              ),
-            ),
-            Container(
-              color: Colors.yellow,
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              child: const Text(
-                "1",
-              ),
-            ),
-          ],
->>>>>>> fb51c4e98c4af622bb89ec2d6fbf8492f83fbee2
-        ));
+      ),
+    );
   }
 }
